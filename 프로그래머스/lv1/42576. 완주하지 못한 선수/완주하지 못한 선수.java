84e43069baf20b6b12e2d812651580dfb348fab8
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-16 19:47:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.98 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,35 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        for(int i = 0 ; i<completion.length;i++){
-            if(!completion[i].equals(participant[i])){
-                return participant[i];
+        String answer = "";
+           HashMap<String,Integer> hm = new HashMap<>();
+            for(String p : participant){
+                hm.put( p , hm.getOrDefault(p,0)+1 );
+            }
+            for(String c : completion){
+                hm.put( c , hm.get(c)-1 );
+            }
         
-                
-            }       
+            for(String key : hm.keySet()){
+                if(hm.get(key)!=0){
+                    answer = key;
+                }
+            }
             
-        }
-        return participant[participant.length-1];
+            return answer;
+        
+        
+        
+
     }
 }
+
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
+//         for(int i = 0 ; i<completion.length;i++){
+//             if(!completion[i].equals(participant[i])){
+//                 return participant[i];
+//             }       
+            
+//         }
+//         return participant[participant.length-1];
