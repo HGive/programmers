d1c511bdab95bca9c473aa4163db49f517b5be52
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-16 17:01:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -7,7 +7,6 @@ class Solution {
                 if(s.charAt(i)>'9'||s.charAt(i)<'0'){
                     
                     answer=false;
-                    break;
                 }
             }
         }else{
