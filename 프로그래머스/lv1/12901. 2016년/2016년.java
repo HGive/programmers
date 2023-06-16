cf61f8ff7caa80c31c1b1a558bd89cb25a9a8727
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-16 16:21:57 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,7 +1,14 @@
 import java.time.*;
 class Solution {
     public String solution(int a, int b) {
-        
-        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
+        String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int allDate=0;
+        for(int i = 0 ; i < a-1 ; i++){
+            allDate+=date[i];
+        }
+        allDate+=b-1;
+        return day[allDate%7];
+        // return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
     }
 }
