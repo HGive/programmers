985c28aa979286c28ccbdfa6bdb5a1a8e32e962f
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-16 14:23:40 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,12 +1,13 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
         int sum = 0;
         for(int i = 0 ; i<10;i++){
             sum+=i;
         }
-        for(int i : numbers){
-            sum-=i;
+        for(int a : numbers){
+            sum-=a;
         }
         return sum;
     }
