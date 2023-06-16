523027bde5a9b655230a95153ff8e0259ba761f9
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-16 13:52:09 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String code) {
+        String ret = "";
+        int mode = 0;
+        for(int i =0; i<code.length();i++){
+            if(code.charAt(i)=='1'){
+                mode=1-mode;
+            }
+            else{
+                if(mode==0&&i%2==0){
+                    ret+=code.charAt(i);
+                }else if(mode==1&&i%2==1){
+                    ret+=code.charAt(i);
+                }
+            }      
+        }
+        if(ret.equals("")){
+            return "EMPTY";
+        }else{
+            return ret;
+        }
+        
+        
+    }
+}
