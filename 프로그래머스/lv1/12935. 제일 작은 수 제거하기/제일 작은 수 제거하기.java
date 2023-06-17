d2bdaad19e0d5580f5d460ff6515da2ba076a094
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-17 09:50:05 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.84 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -4,11 +4,12 @@ class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
         if(arr.length==1){
-            answer = new int[]{-1};
+            arr[0] = -1;
+            return arr;
         }else{
             ArrayList<Integer> list = new ArrayList<>();
-            for(int i = 0 ; i < arr.length; i++){
-                list.add(arr[i]);
+            for(int a : arr){
+                list.add(a);
             }
             list.remove(Collections.min(list));
             answer =new int[list.size()];
