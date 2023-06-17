39d62d2b5ae65911735e9660074800c46aba766f
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-17 13:40:58 +0900

    [level 1] Title: 3진법 뒤집기, Time: 23.68 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String temp= "";
+        while(n>0){
+            temp+=n%3;
+            n/=3;
+        }
+         // temp = "0021"
+        answer = Integer.parseInt(temp, 3);
+
+        return answer;
+    }
+}
