c8808491432a6771c0ac58ffe62ddcbe716b2d94
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-19 11:58:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.37 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,23 +3,16 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-        for(int i = 0; i<arr.length;i++){
-            list.add(arr[i]);
+        list.add(arr[0]);
+        for(int i = 1; i<arr.length;i++){
+            if(arr[i]!=arr[i-1]){
+                list.add(arr[i]);
+            }
         }
-        for(int i = 0; i<arr.length-1;i++){
-             if(list.get(i)==list.get(i+1)){
-               continue;
-                 
-             }else{
-                 list2.add(list.get(i));
-             }
-         }
-        int[] answer = new int[list2.size()+1];
-        for(int i= 0 ; i<list2.size();i++){
-            answer[i]=list2.get(i);
+        int[] answer = new int[list.size()];
+        for(int i = 0 ; i<list.size();i++){
+            answer[i]=list.get(i).intValue();
         }
-        answer[list2.size()]=list.get(list.size()-1);
         return answer;
     }
 }
