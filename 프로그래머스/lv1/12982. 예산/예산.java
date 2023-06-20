58a8d3cb58298b60a9bb42843c8968e70e223f4b
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-20 20:48:09 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -2,13 +2,11 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         Arrays.sort(d);
-        int sum = 0 ;
         int cnt = 0 ;
         for(int a : d){
-            if(budget-sum>=a){
-                sum+=a;    
-                cnt++;
-            }
+            budget-=a;
+            if(budget<0)break;
+            cnt++;
         }
         return cnt;
     }
