e91a77a1e56ad7893a56bb9ef9cdd1f3d3bb2339
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-21 00:49:33 +0900

    [level 1] Title: 소수 찾기, Time: 14.03 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -3,17 +3,17 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         boolean[] temp = new boolean[n+1];
-        Arrays.fill(temp,true);
-        temp[0]=false;
-        temp[1]=false;
+        // Arrays.fill(temp,true);
+        temp[0]=true;
+        temp[1]=true;
         for(int i =2 ; i<=Math.sqrt(n);i++){
-            if(!temp[i])continue;
+            if(temp[i])continue;
             for(int j = 2*i; j<=n ; j+=i){
-                temp[j]=false;
+                temp[j]=true;
             }
         }
         for(boolean prime : temp){
-            if(prime)answer++;
+            if(!prime)answer++;
         }
         return answer;
     }
