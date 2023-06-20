d2016089445b7dced89d6e7290502ef4fe43719d
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-20 20:28:34 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -2,29 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        Map<Integer,Boolean> hm = new HashMap<>();
         for(int i = left ; i<=right;i++){
             int cnt = 0;
-            boolean flag = true;
             for(int j = 1; j<=i; j++){
                 if(i%j==0){
                     cnt++;
                 }
             }   
             if(cnt%2==0){
-                    flag = true;
+                    answer += i;
                 }else{
-                    flag = false;
-            }
-            hm.put(i,flag);
-        }
-        for(Map.Entry<Integer,Boolean> entry : hm.entrySet()){
-            if(entry.getValue()){
-                answer+=entry.getKey();
-            }else{
-                answer-=entry.getKey();
+                    answer -= i ;
             }
         }
+        
         return answer;
     }
 }
