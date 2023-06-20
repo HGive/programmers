6a9318c70c0e78c904cbb84709ecc085e684b980
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-20 22:50:40 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,13 +1,7 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int lcm = Math.max(m,n);
-        for(int gcd=Math.min(n,m) ;gcd>=1;gcd--){
-            if(n%gcd==0&&m%gcd==0){
-                answer[0]=gcd;
-                break;
-            }
-        }
+        answer[0]= gcd(n,m);
         answer[1]= n*m/answer[0];
         // while(true){
         //     if(lcm%n==0&&lcm%m==0){
@@ -18,5 +12,9 @@ class Solution {
         //     }
         // }
         return answer;
+    }
+    public int gcd(int p, int q){
+        if(q==0){return p;}
+           return gcd(q,p%q);
     }
 }
