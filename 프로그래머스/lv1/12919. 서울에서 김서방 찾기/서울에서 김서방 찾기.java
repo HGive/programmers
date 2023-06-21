eca66d4126c4988034ccd35ef68ead191e03bb25
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-21 20:22:28 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.60 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,13 +2,20 @@ class Solution {
     public String solution(String[] seoul) {
         String answer = "";
         int cnt = 0;
-        for(String s : seoul){
+        for(int i = 0 ; i<seoul.length;i++){
+            if(seoul[i].equals("Kim")) {
+            cnt=i;
+            break;    
+            }
             
-            if(s.equals("Kim")){
-                break;
-            };
-            cnt++;
         }
+//         for(String s : seoul){
+            
+//             if(s.equals("Kim")){
+//                 break;
+//             };
+//             cnt++;
+//         }
         answer = "김서방은 "+ cnt +"에 있다";
         return answer;
     }
