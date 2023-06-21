79a807c3f9c81bac43321b562f3f8dcdc606c210
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-21 20:54:32 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,14 +1,24 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        String temp= "";
-        while(n>0){
-            temp=temp + n%3 ;
-            n/=3;
-        }
-         // temp = "0021"
-        answer = Integer.parseInt(temp, 3);
+        StringBuilder sb = new StringBuilder();
+            while(n>0){
+                sb.append(String.valueOf(n%3));
+                n/=3;
+            }
+            return Integer.parseInt(sb.toString(),3);
+        
+        
+        
+        
+//         int answer = 0;
+//         String temp= "";
+//         while(n>0){
+//             temp=temp + n%3 ;
+//             n/=3;
+//         }
+//          // temp = "0021"
+//         answer = Integer.parseInt(temp, 3);
 
-        return answer;
+//         return answer;
     }
 }
