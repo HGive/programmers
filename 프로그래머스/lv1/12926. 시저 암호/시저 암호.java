837eb2bfc61d85f37845351ce8d210ad9c2627a6
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-21 21:43:48 +0900

    [level 1] Title: 시저 암호, Time: 1.62 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,33 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        char[] ch = s.toCharArray();
-        for(char c : ch){
-            if(c==' '){
-              answer+=c;
-              continue;
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i =0 ; i <s.length();i++){
+            char ch = s.charAt(i);
+            if(ch==' '){sb.append(' ');continue;}
+            if(ch>='A'&&ch<='Z'){
+                sb.append((char) ('A'+(ch-'A'+n)%26));
+            }else if(ch>'Z'&&ch<='z'){
+                sb.append((char) ('a'+(ch-'a'+n)%26));
             }
-            if(c>='a'&&c<='z'){
-                c=(char)('a'+(c+n-'a')%26);
-            }else if(c>='A'&&c<='Z'){
-                c=(char)('A'+(c+n-'A')%26);
-            }
-            answer+=c;
         }
-        return answer;
+
+        return sb.toString();
+        // String answer = "";
+        // char[] ch = s.toCharArray();
+        // for(char c : ch){
+        //     if(c==' '){
+        //       answer+=c;
+        //       continue;
+        //     }
+        //     if(c>='a'&&c<='z'){
+        //         c=(char)('a'+(c+n-'a')%26);
+        //     }else if(c>='A'&&c<='Z'){
+        //         c=(char)('A'+(c+n-'A')%26);
+        //     }
+        //     answer+=c;
+        // }
+        // return answer;
     }
 
 }
