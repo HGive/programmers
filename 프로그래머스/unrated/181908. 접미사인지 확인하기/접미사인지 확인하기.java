2a3b65d6d2f608178bd819254e6b803b9912cc37
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-25 03:03:17 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        ArrayList<String> list = new ArrayList<>();
-        for(int i = 0 ; i<my_string.length();i++){
-            list.add(my_string.substring(my_string.length() -i-1 , my_string.length()));
-        }
+//         ArrayList<String> list = new ArrayList<>();
+//         for(int i = 0 ; i<my_string.length();i++){
+//             list.add(my_string.substring(my_string.length() -i-1 , my_string.length()));
+//         }
         
-        return list.contains(is_suffix)?1:0;
+        return my_string.endsWith(is_suffix)?1:0;
     }
 }
