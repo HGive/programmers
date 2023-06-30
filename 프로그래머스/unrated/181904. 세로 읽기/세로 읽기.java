2860ae93618cf851b54089df6b3aee25ca3ad50d
algorijun <123007169+HGive@users.noreply.github.com>
2023-07-01 01:25:02 +0900

    [unrated] Title: 세로 읽기, Time: 0.09 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,17 +1,23 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
         StringBuilder sb = new StringBuilder();
-        int l = my_string.length()/m;
-        char[][] arr = new char[l][m];
-        for(int i= 0; i<l ; i++){
-            for(int j = 0; j<m;j++){
-                arr[i][j]=my_string.charAt(i*m+j);
-            }
-        }
-        
-        for(int i = 0 ; i<l;i++){
-            sb.append(arr[i][c-1]);
-        }
+        for(int i = c-1 ; i<my_string.length();i+=m){
+            sb.append(my_string.charAt(i));
+        }        
         return sb.toString();
     }
 }
+
+
+// int l = my_string.length()/m;
+//         char[][] arr = new char[l][m];
+//         for(int i= 0; i<l ; i++){
+//             for(int j = 0; j<m;j++){
+//                 arr[i][j]=my_string.charAt(i*m+j);
+//             }
+//         }
+        
+//         for(int i = 0 ; i<l;i++){
+//             sb.append(arr[i][c-1]);
+//         }
+//         return sb.toString();
