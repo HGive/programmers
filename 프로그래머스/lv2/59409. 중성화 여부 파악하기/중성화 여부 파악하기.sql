334e6c66f4026fa23ade66ec56e6a6de4b1434a1
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-30 15:29:00 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,9 +1,8 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME,
 Case
-when SEX_UPON_INTAKE like 'Spayed%' then 'O'
-when SEX_UPON_INTAKE like 'Neutered%' then 'O'
+when SEX_UPON_INTAKE like 'Spayed%' or SEX_UPON_INTAKE like 'Neutered%' then 'O'
 else 'X'
 end
 from ANIMAL_INS 
-order by ANIMAL_ID 
+order by ANIMAL_ID
