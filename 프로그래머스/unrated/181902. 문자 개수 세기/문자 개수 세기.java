7d7ae347f9dc3e50d1502c34de4dbc4100e4cc67
algorijun <123007169+HGive@users.noreply.github.com>
2023-07-01 22:29:16 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,24 +1,32 @@
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
-        for(int i =0 ; i<26;i++){
-            int cnt = 0;
-            for(int j = 0; j<my_string.length();j++){
-                if(my_string.charAt(j)-0=='A'-0+i){
-                    cnt++;
-                }
+        for(int i =0; i<my_string.length();i++){
+            char c = my_string.charAt(i);
+            if(c>='a'){
+                answer[c-'a'+26]++;
+            }else{
+                answer[c-'A']++;
             }
-            answer[i]=cnt;
         }
-        for(int i =26 ; i<52;i++){
-            int cnt = 0;
-            for(int j = 0; j<my_string.length();j++){
-                if(my_string.charAt(j)-0=='a'-0+i-26){
-                    cnt++;
-                }
-            }
-            answer[i]=cnt;
-        }  
         return answer;
     }
 }
+// for(int i =0 ; i<26;i++){
+//             int cnt = 0;
+//             for(int j = 0; j<my_string.length();j++){
+//                 if(my_string.charAt(j)-0=='A'-0+i){
+//                     cnt++;
+//                 }
+//             }
+//             answer[i]=cnt;
+//         }
+//         for(int i =26 ; i<52;i++){
+//             int cnt = 0;
+//             for(int j = 0; j<my_string.length();j++){
+//                 if(my_string.charAt(j)-0=='a'-0+i-26){
+//                     cnt++;
+//                 }
+//             }
+//             answer[i]=cnt;
+//         }  
