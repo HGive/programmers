5c1e95bb53f159a4b9c1e8cc1da2e60b2748d328
algorijun <123007169+HGive@users.noreply.github.com>
2023-07-04 17:04:11 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,10 +1,8 @@
 class Solution {
     public int[] solution(int start, int end) {
         int[] answer = new int[start-end+1];
-        int cnt = 0;
-        for(int i = start ; i >= end ; i--){
-            answer[cnt]=i;
-            cnt++;
+        for(int i = 0 ; i <start-end+1 ; i++){
+            answer[i]=start-i;
         }
         return answer;
     }
