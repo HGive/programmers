0a8eab7c017170e63e6801f0d7ea4bb1e4c0378c
algorijun <123007169+HGive@users.noreply.github.com>
2023-07-04 16:32:00 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,16 +1,9 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, int k) {
-        
-        List<Integer> list = new ArrayList<>();
-        int s =1;
-        while(k*s<=n){
-            list.add(k*s);
-            s++;
-        }
-        int[] answer = new int[list.size()];
-        for(int i = 0 ; i<list.size();i++){
-            answer[i]=list.get(i);
+        int[] answer=new int[n/k];
+        for(int i = 1 ;i <= n/k ; i++){
+            answer[i-1]=k*i;
         }
         return answer;
     }
