1fbccb0acf8f3b3c917d49c9247975f050632deb
algorijun <123007169+HGive@users.noreply.github.com>
2023-07-04 16:48:46 +0900

    [unrated] Title: 글자 지우기, Time: 0.50 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -1,16 +1,12 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        String answer = "";
         StringBuilder sb = new StringBuilder();
-        String[] arr = new String[my_string.length()];
-        for(int i = 0 ; i<my_string.length();i++){
-            arr[i]=my_string.substring(i,i+1);
-        }
+        String[] arr = my_string.split("");
         for(int dice : indices){
             arr[dice]="";
         }
-        for(String c : arr){
-            sb.append(c);
+        for(String s : arr){
+            sb.append(s);
         }
         return sb.toString();
     }
