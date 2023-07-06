c0835fb27a18f116190a7627d5fc32ee7c12c69f
algorijun <123007169+HGive@users.noreply.github.com>
2023-07-06 22:48:44 +0900

    [unrated] Title: 리스트 자르기, Time: 2.75 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -1,33 +1,45 @@
 import java.util.*;
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] answer = {};
         switch(n){
             case 1:
-                for(int i = 0 ; i<= slicer[1] ; i++){
-                    list.add(num_list[i]);
-                }
-                break;
-            case 2:
-                for(int i = slicer[0] ; i< num_list.length ; i++){
-                    list.add(num_list[i]);
-                }
-                break;
-            case 3:
-                for(int i = slicer[0] ; i<= slicer[1] ; i++){
-                    list.add(num_list[i]);
-                }
-                break;
-            case 4:
-                for(int i = slicer[0] ; i<= slicer[1] ; i+=slicer[2]){
-                    list.add(num_list[i]);
-                }
-                break;
-        }
-        int[] answer =new int[list.size()];
-        for(int i = 0 ; i<list.size();i++){
-            answer[i]=list.get(i);
+            answer = Arrays.copyOf(num_list, slicer[1] + 1);
+            break;
+        case 2:
+            answer = Arrays.copyOfRange(num_list, slicer[0], num_list.length);
+            break;
+        case 3:
+            answer = Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1);
+            break;
+        case 4:
+            answer = IntStream
+                .range(0, (slicer[1] - slicer[0]) / slicer[2] + 1)
+                .map(i -> num_list[slicer[0] + i * slicer[2]]).toArray();
+            break;
         }
         return answer;
     }
 }
+// case 1:
+//                 for(int i = 0 ; i<= slicer[1] ; i++){
+//                     list.add(num_list[i]);
+//                 }
+//                 break;
+//             case 2:
+//                 for(int i = slicer[0] ; i< num_list.length ; i++){
+//                     list.add(num_list[i]);
+//                 }
+//                 break;
+//             case 3:
+//                 for(int i = slicer[0] ; i<= slicer[1] ; i++){
+//                     list.add(num_list[i]);
+//                 }
+//                 break;
+//             case 4:
+//                 for(int i = slicer[0] ; i<= slicer[1] ; i+=slicer[2]){
+//                     list.add(num_list[i]);
+//                 }
+//                 break;
+// 
