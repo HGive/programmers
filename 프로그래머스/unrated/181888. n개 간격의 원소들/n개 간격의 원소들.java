7d1b088dbc442d44703ea9663d4ac7aeed079b54
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-04 11:10:29 +0900

    [unrated] Title: n개 간격의 원소들, Time: 3.06 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 0; i < num_list.length; i+=n) {
+            resultList.add(num_list[i]);
+        }
+        int[] resultArray = resultList.stream().mapToInt(Integer::intValue).toArray();
+        return resultArray;
+    }
+}
