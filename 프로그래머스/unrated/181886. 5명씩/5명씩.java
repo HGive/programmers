31343c6d84b6073f8ab1fd4de8df4ef8813a599e
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-07 00:06:12 +0900

    [unrated] Title: 5명씩, Time: 0.20 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] names) {
-        List<String> list = new ArrayList<>();
-        for(int i = 0 ;i<names.length;i++){
-            if(i%5==0){
-                list.add(names[i]);
-            }
+        int idx=0;
+        int length =(int) Math.ceil(names.length/5.0);
+        String[] answer = new String[length];
+        for(int i = 0 ;i<names.length;i+=5){
+            answer[idx++]=names[i];
         }
-        return list.toArray(new String[0]);
+        return answer;
     }
 }
