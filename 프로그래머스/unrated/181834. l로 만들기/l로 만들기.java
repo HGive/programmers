8af9499358aa689c81c7a17f26946f8799369662
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-26 09:51:43 +0900

    [unrated] Title: l로 만들기, Time: 15.01 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,14 +1,7 @@
 import java.util.*;
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
-        for(char c : myString.toCharArray()){
-            if(c<'l'){
-                sb.append("l");
-            }else{
-                sb.append(c);
-            }
-        }
-        return sb.toString();
+        String answer = myString.replaceAll("[^l-z]","l");
+        return answer;
     }
 }
