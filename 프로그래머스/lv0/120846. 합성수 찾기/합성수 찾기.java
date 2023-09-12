c0f263803f05d5c9258424605a57844b09585a85
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-12 10:24:49 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int cnt=0;
         for(int i =4;i<=n;i++){
-            for(int j = 1 ;j<=i;j++){
+            for(int j = 2 ;j<=i/2;j++){
                 if(i%j==0){
-                    cnt++;
-                }
-                if(cnt==3){
-                    cnt=0;
                     answer++;
                     break;
                 }
