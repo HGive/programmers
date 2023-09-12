48f197092c86c24bafaa799289b9a1f3ce29b148
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-12 10:53:34 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int n) {
-        if(n==2){
-            return 2;
+        int answer =1;
+        int fac = 1; 
+        while(n>=fac){
+            fac*=answer;
+            answer++;
         }
-        int answer=1;
-        for(int i =1;i<=n;i++){
-            answer*=i;
-            if(answer>n){
-                return i-1;
-            }
-        }
-        return answer;
+        return answer-2;
     }
 }
