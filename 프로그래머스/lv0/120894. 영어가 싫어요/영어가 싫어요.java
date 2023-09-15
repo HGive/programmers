c6cb7238fcb18365231306f0b4ca82e7ad55e60e
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-15 09:46:56 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.63 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,20 +1,13 @@
 import java.util.*;
 class Solution {
     public long solution(String numbers) {
-         Map<String, Long> wordToNum = new HashMap<>();
-        wordToNum.put("zero", 0L);
-        wordToNum.put("one", 1L);
-        wordToNum.put("two", 2L);
-        wordToNum.put("three", 3L);
-        wordToNum.put("four", 4L);
-        wordToNum.put("five", 5L);
-        wordToNum.put("six", 6L);
-        wordToNum.put("seven", 7L);
-        wordToNum.put("eight", 8L);
-        wordToNum.put("nine", 9L);
-        for(String key : wordToNum.keySet()){
-            numbers=numbers.replace(key,String.valueOf(wordToNum.get(key)));
-        }
-        return Long.parseLong(numbers);
+        String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+            for (int i = 0; i < numbers_arr.length; i++) {
+                numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
+            }
+
+            long answer = Long.parseLong(numbers);
+            return answer;
     }
 }
