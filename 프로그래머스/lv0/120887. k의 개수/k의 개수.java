d1c9ef418498463ead35306a0a6db597bcbd8291
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-19 18:28:58 +0900

    [level 0] Title: k의 개수, Time: 16.29 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,16 +1,10 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer=0;
         StringBuilder sb = new StringBuilder();
         for(int start = i; start<=j;start++){
             sb.append(String.valueOf(start));
         }
-        String[] numStr = sb.toString().split("");
-        for(String num : numStr){
-            if(num.equals(k+"")){
-                answer++;
-            }
-        }
-        return answer;
+        String temp = sb.toString();
+        return temp.length()- temp.replace(k+"","").length();
     }
 }
