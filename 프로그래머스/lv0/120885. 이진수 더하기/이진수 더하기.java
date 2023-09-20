a33e7cea35e29a7aa1f243b710d43ec052a72791
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-20 11:43:48 +0900

    [level 0] Title: 이진수 더하기, Time: 0.05 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,11 +1,6 @@
 import java.math.BigInteger;
 class Solution {
     public String solution(String bin1, String bin2) {
-        // 문자열로 표현된 이진수를 BigInteger로 변환
-        BigInteger num1 = new BigInteger(bin1, 2);
-        BigInteger num2 = new BigInteger(bin2, 2);
-
-        // 두 수를 더한 결과를 이진수 문자열로 변환
-        return num1.add(num2).toString(2);
+        return Integer.toBinaryString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2));
     }
 }
