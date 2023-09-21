d4e4ac41b73cb82f2fb6fba0f2f939e54cf77066
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-21 17:50:37 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -1,33 +1,7 @@
 class Solution {
     public int solution(int[][] dots) {
-        int width = 0;
-        int height = 0;
-
-        // 가로 길이 찾기
-        for (int i = 0; i < 4; i++) {
-            for (int j = i + 1; j < 4; j++) {
-                if (dots[i][1] == dots[j][1]) {
-                    width = Math.abs(dots[i][0] - dots[j][0]);
-                    break;
-                }
-            }
-            if (width != 0) {
-                break;
-            }
-        }
-
-        // 세로 길이 찾기
-        for (int i = 0; i < 4; i++) {
-            for (int j = i + 1; j < 4; j++) {
-                if (dots[i][0] == dots[j][0]) {
-                    height = Math.abs(dots[i][1] - dots[j][1]);
-                    break;
-                }
-            }
-            if (height != 0) {
-                break;
-            }
-        }
+        int width = Math.max(Math.abs(dots[1][0]-dots[2][0]),Math.abs(dots[1][0]-dots[3][0]));
+        int height = Math.max(Math.abs(dots[1][1]-dots[2][1]),Math.abs(dots[1][1]-dots[3][1]));
 
         return width * height;
     }
