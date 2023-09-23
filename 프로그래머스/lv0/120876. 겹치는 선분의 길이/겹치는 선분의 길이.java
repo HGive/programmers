781ca308f01ed2f172811f830b6d0842bb2f70ea
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-23 16:34:09 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,17 +1,31 @@
+import java.util.*;
 class Solution {
     public int solution(int[][] lines) {
-        int answer = 0;
-        int[] arr = new int[200];
-        for(int[] line:lines){
-            for(int i = line[0]+100;i<line[1]+100;i++){
-                arr[i]++;
+        int answer=0;
+        Map<Integer,Integer> hm = new HashMap<>();
+        for(int[] line :lines){
+            for(int i = line[0];i<line[1];i++){
+                hm.put(i,hm.getOrDefault(i,0)+1);
             }
         }
-        for(int dot : arr){
-            if(dot>1){
+        for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
+            if(entry.getValue()>1){
                 answer++;
             }
         }
         return answer;
+        // int answer = 0;
+        // int[] arr = new int[200];
+        // for(int[] line:lines){
+        //     for(int i = line[0]+100;i<line[1]+100;i++){
+        //         arr[i]++;
+        //     }
+        // }
+        // for(int dot : arr){
+        //     if(dot>1){
+        //         answer++;
+        //     }
+        // }
+        // return answer;
     }
 }
