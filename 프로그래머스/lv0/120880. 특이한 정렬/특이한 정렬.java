4bf1efe96dafbc004bcfc17a15acb4bc00a7a320
algorijun <123007169+HGive@users.noreply.github.com>
2023-09-27 21:10:33 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,20 +1,21 @@
+
 import java.util.*;
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        Map<Integer,Integer> hm = new HashMap<>();
-        for(int i = 0; i<numlist.length;i++){
-            hm.put(numlist[i],Math.abs(n-numlist[i]));
+
+        int size = numlist.length;
+        for(int i=0; i<size-1; i++){
+            for(int k=i+1; k<size; k++){
+                int a = (numlist[i] - n) * (numlist[i] > n ? 1 : -1);
+                int b = (numlist[k] - n) * (numlist[k] > n ? 1 : -1);
+                if(a > b || (a == b && numlist[i] < numlist[k])){
+                    int temp = numlist[i];
+                    numlist[i] = numlist[k];
+                    numlist[k] = temp;
+                }
+
+            }
         }
-        List<Map.Entry<Integer,Integer>> entryList = new ArrayList<>(hm.entrySet());
-        Collections.sort(entryList,new Comparator<Map.Entry<Integer,Integer>>(){
-            public int compare(Map.Entry<Integer, Integer> a, Map.Entry<Integer, Integer> b) {
-    if (!a.getValue().equals(b.getValue())) {
-        return a.getValue().compareTo(b.getValue());
-    } else {
-        return b.getKey().compareTo(a.getKey());
-    }
-}
-        });
-        return entryList.stream().mapToInt(Map.Entry::getKey).toArray();
+        return numlist;
     }
 }
