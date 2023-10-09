90152b8c7f4a29ed207711dd295d83071e433b18
algorijun <123007169+HGive@users.noreply.github.com>
2023-10-09 16:02:16 +0900

    [level 0] Title: 등수 매기기, Time: 0.34 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,21 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
-        Map<Integer,List<Integer>> hm = new HashMap<>();
-        for(int i = 0 ;i<score.length;i++){
-            hm.computeIfAbsent(score[i][0]+score[i][1], k -> new ArrayList<>()).add(i+1);
+        List<Integer> scoreList = new ArrayList<>();
+        for(int i = 0 ; i<score.length;i++){
+            scoreList.add(score[i][0]+score[i][1]);
         }
-        List<Integer> sortedScores = new ArrayList<>(hm.keySet());
-        Collections.sort(sortedScores, Collections.reverseOrder());
-        
-        int rank = 1;
-        for(int totalScore : sortedScores){
-            List<Integer> students = hm.get(totalScore);
-            for(int student : students){
-                answer[student-1]=rank;
-            }
-            rank+= students.size();
+        Collections.sort(scoreList,Collections.reverseOrder());
+        int[] answer = new int[score.length];
+        for(int i = 0 ; i<score.length;i++){
+            answer[i] = scoreList.indexOf(score[i][0]+score[i][1])+1;
         }
         return answer;
     }
