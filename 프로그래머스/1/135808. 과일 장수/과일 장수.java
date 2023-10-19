f5350f1d53e9ba09c8d6876e1e38b2dd2fc7c253
algorijun <123007169+HGive@users.noreply.github.com>
2023-10-19 10:15:46 +0900

    [level 1] Title: 과일 장수, Time: 0.41 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,10 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        if(score.length<m)return 0;
-        int answer = 0;
-        List<Integer> scoreList = new ArrayList<>();
-        for(int num : score){
-            scoreList.add(num);
-        }
-        Collections.sort(scoreList,Collections.reverseOrder());
-        for(int i = m-1 ; i<score.length;i+=m){
-            answer+=scoreList.get(i)*m;
+        int answer = 0; 
+        Arrays.sort(score);
+        for(int i = score.length-m;i>=0;i-=m){
+            answer+= score[i]*m;
         }
         return answer;
     }
