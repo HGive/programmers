ea849b76def34ada4e81f80b714eefab356e6c92
algorijun <123007169+HGive@users.noreply.github.com>
2023-10-27 12:04:35 +0900

    [level 1] Title: 문자열 나누기, Time: 0.64 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(String s) {
         char x = s.charAt(0);
-        int answer = 1;
+        int answer = 0;
         int first = 0;
         int diff = 0;
-        for(int i = 0 ; i < s.length()-1 ; i++){
+        for(int i = 0 ; i < s.length() ; i++){
+            if(first == diff){
+                answer++;
+                x=s.charAt(i);
+            }
             if(x!=s.charAt(i)){
                 diff++;
             }else{
                 first++;
-            }
-            if(first == diff){
-                answer++;
-                x=s.charAt(i+1);
             }
         }
         return answer;
