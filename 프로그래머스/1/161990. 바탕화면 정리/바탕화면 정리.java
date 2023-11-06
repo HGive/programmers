0e15eb4a2f09a2c5194b2cc3f0c59d887f473307
algorijun <123007169+HGive@users.noreply.github.com>
2023-11-06 16:43:03 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.08 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux= 50;
+        int luy= 50;
+        int rdx= 0;
+        int rdy= 0;
+        for(int i = 0 ; i<wallpaper.length;i++){
+            String file = wallpaper[i];
+            int start = file.indexOf("#");
+            int end = file.lastIndexOf("#");
+            if(start!=-1){
+                if(i<=lux) lux = i;
+                if(start<=luy){
+                    luy = start;
+                }
+                if(end>=rdy){
+                    rdy = end;
+                }
+                if(i>=rdx){
+                    rdx = i;
+                }
+            }
+        }
+        int[] answer = new int[]{lux,luy,rdx+1,rdy+1};
+        return answer;
+    }
+}
